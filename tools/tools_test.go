e062fa49c229a985158f817a13751120ccdd6a18
SealTV <seal.matob@gmail.com>
2017-09-22 00:03:16 +0300

    Refactoring

--- tools/tools_test.go
+++ tools/tools_test.go
@@ -2,20 +2,18 @@ package tools
 
 import "testing"
 
-type sumTestParams struct {
-	a int
-	b int
-	s int
-}
-
-var sumTests = []sumTestParams{
-	{0, 0, 0},
-	{1, 0, 1},
-	{0, 1, 1},
-	{1, -1, 0},
-}
-
 func TestSum(t *testing.T) {
+	var sumTests = []struct {
+		a int
+		b int
+		s int
+	}{
+		{0, 0, 0},
+		{1, 0, 1},
+		{0, 1, 1},
+		{1, -1, 0},
+	}
+
 	for _, data := range sumTests {
 		s := Sum(data.a, data.b)
 		if s != data.s {
@@ -24,26 +22,23 @@ func TestSum(t *testing.T) {
 	}
 }
 
-type testpair struct {
-	value  int
-	result int
-}
-
-var fibonacciTests = []testpair{
-	{0, 0},
-	{1, 1},
-	{2, 1},
-	{3, 2},
-	{4, 3},
-	{5, 5},
-	{6, 8},
-	{7, 13},
-	{8, 21},
-	{9, 34},
-	{10, 55},
-}
-
 func TestFibonacci(t *testing.T) {
+	var fibonacciTests = []struct {
+		value  int
+		result int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 1},
+		{3, 2},
+		{4, 3},
+		{5, 5},
+		{6, 8},
+		{7, 13},
+		{8, 21},
+		{9, 34},
+		{10, 55},
+	}
 	for _, data := range fibonacciTests {
 		s := Fibonacci(data.value)
 		if s != data.result {
@@ -52,21 +47,23 @@ func TestFibonacci(t *testing.T) {
 	}
 }
 
-var factorialTests = []testpair{
-	{0, 1},
-	{1, 1},
-	{2, 2},
-	{3, 6},
-	{4, 24},
-	{5, 120},
-	{6, 720},
-	{7, 5040},
-	{8, 40320},
-	{9, 362880},
-	{10, 3628800},
-}
-
 func TestFactorial(t *testing.T) {
+	var factorialTests = []struct {
+		value  int
+		result int
+	}{
+		{0, 1},
+		{1, 1},
+		{2, 2},
+		{3, 6},
+		{4, 24},
+		{5, 120},
+		{6, 720},
+		{7, 5040},
+		{8, 40320},
+		{9, 362880},
+		{10, 3628800},
+	}
 	for _, data := range factorialTests {
 		s := Factorial(data.value)
 		if s != data.result {
