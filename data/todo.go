42d28707468535908feced058166d0e7ef3cea14
SealTV <seal.matob@gmail.com>
2017-09-29 23:54:55 +0300

    Добавил методы для работы с таблицой todos

--- data/todo.go
+++ data/todo.go
@@ -3,6 +3,7 @@ package data
 import (
 	"database/sql"
 	_ "github.com/lib/pq"
+	"log"
 	"time"
 )
 
@@ -21,33 +22,63 @@ type TodoCollection struct {
 }
 
 func GetAllTodos(db *sql.DB) TodoCollection {
-	s := "SELECT * FROM todos"
-	rows, err := db.Query(s)
+	rows, err := db.Query("SELECT * FROM todos")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer rows.Close()
 	return parseTodoRows(rows)
 }
 
 func GetAllTodosForUser(db *sql.DB, user User) TodoCollection {
-	s := "SELECT * FROM todos WHERE user_id = $1"
-	rows, err := db.Query(s, user.Id)
+	rows, err := db.Query("SELECT * FROM todos WHERE user_id = $1", user.Id)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer rows.Close()
 	return parseTodoRows(rows)
 }
 
 func GetAllTodosForUserList(db *sql.DB, user User, list List) TodoCollection {
-	s := "SELECT * FROM todos WHERE user_id = $1  AND list_id = $2"
-	rows, err := db.Query(s, user.Id, list.Id)
+	rows, err := db.Query("SELECT * FROM todos WHERE user_id = $1 AND list_id = $2", user.Id, list.Id)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer rows.Close()
 	return parseTodoRows(rows)
+}
+
+func AddTodo(db *sql.DB, todo Todo) Todo {
+	err := db.QueryRow(`INSERT
+			INTO todos(title, description, list_id, is_active, user_id)
+			VALUES($1, $2, $3, $4, $5)
+			RETURNING id, date_create;`,
+		todo.Title, todo.Description, todo.ListId, todo.IsActive, todo.UserId).Scan(&todo.Id, &todo.DateCreate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return todo
+}
+
+func UpdateTodo(db *sql.DB, todo Todo) (int64, error) {
+	r, err := db.Exec(
+		`UPDATE todos
+				SET title  = $2, description = $3, list_id = $4, is_active = $5, user_id = $6
+				WHERE id = $1`,
+		todo.Id, todo.Title, todo.Description, todo.ListId, todo.IsActive, todo.UserId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return r.RowsAffected()
+}
+
+func DeleteTodo(db *sql.DB, todo Todo) (int64, error) {
+	r, err := db.Exec(`DELETE FROM todos WHERE id = $1`, todo.Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return r.RowsAffected()
 }
 
 func parseTodoRows(rows *sql.Rows) TodoCollection {
@@ -57,7 +88,7 @@ func parseTodoRows(rows *sql.Rows) TodoCollection {
 
 		err := rows.Scan(&todo.Id, &todo.Title, &todo.Description, &todo.ListId, &todo.IsActive, &todo.UserId, &todo.DateCreate)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		result.Todo = append(result.Todo, todo)
