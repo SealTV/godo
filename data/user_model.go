e0323bb024a58dc761ae473abb29601bcb37da75
v.tetevin <v.tetevin@corp.mail.ru>
2017-10-20 18:54:05 +0300

    Добавил тест для UserModel

--- data/user_model.go
+++ data/user_model.go
@@ -10,39 +10,35 @@ func (db *postgresConnector) GetUserModel(id int) (model.UserModel, error) {
 		return userModel, err
 	}
 	userModel.User = user
-	rows, err := db.Query(`SELECT list.id, list.name,
-		todo.id, todo.title, todo.description, todo.is_active, todo.date_create
-		FROM lists AS list
-		LEFT JOIN todos AS todo ON list.id = todo.list_id
-		WHERE list.user_id = $1`, id)
+	rows, err := db.Query(`SELECT list.id, list.name, todo.id, todo.title, todo.description, todo.is_active, todo.date_create FROM lists AS list LEFT JOIN todos AS todo ON list.id = todo.list_id WHERE list.user_id = $1`, id)
 	if err != nil {
 		return userModel, err
 	}
 
 	lists := make(map[int]model.TodoListModel)
 	for rows.Next() {
-		var listId int
+		var listID int
 		var listName string
 		var todo model.Todo
 		todo.UserId = user.Id
 
-		err := rows.Scan(&listId, &listName, &todo.Id, &todo.Title, &todo.Description, &todo.IsActive, &todo.DateCreate)
+		err := rows.Scan(&listID, &listName, &todo.Id, &todo.Title, &todo.Description, &todo.IsActive, &todo.DateCreate)
 		if err != nil {
 			return userModel, err
 		}
-		todo.ListId = listId
+		todo.ListId = listID
 
-		list, ok := lists[listId]
+		list, ok := lists[listID]
 		if !ok {
 			list.List = model.List{
-				Id:     listId,
+				Id:     listID,
 				Name:   listName,
 				UserId: user.Id,
 			}
 		}
 
 		list.Todos = append(list.Todos, todo)
-		lists[listId] = list
+		lists[listID] = list
 	}
 
 	for _, v := range lists {
