d2aa0486a8053ea20e44f77ee824da1457516353
v.tetevin <v.tetevin@corp.mail.ru>
2017-10-23 17:25:23 +0300

    Поправил файл с тестами

--- server/auth_test.go
+++ server/auth_test.go
@@ -17,7 +17,7 @@ var (
 	}
 )
 
-func TestServer_register(t *testing.T) {
+func TestServerRegister(t *testing.T) {
 	userJSON := ``
 	e := echo.New()
 	req := httptest.NewRequest(echo.POST, "/", strings.NewReader(userJSON))
@@ -51,7 +51,7 @@ func TestServer_register(t *testing.T) {
 	}
 }
 
-func TestServer_login(t *testing.T) {
+func TestServerLogin(t *testing.T) {
 	type args struct {
 		c echo.Context
 	}
@@ -72,7 +72,7 @@ func TestServer_login(t *testing.T) {
 	}
 }
 
-func TestServer_mainJwt(t *testing.T) {
+func TestServerMainJwt(t *testing.T) {
 	type args struct {
 		c echo.Context
 	}
@@ -88,32 +88,6 @@ func TestServer_mainJwt(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := tt.s.mainJwt(tt.args.c); (err != nil) != tt.wantErr {
 				t.Errorf("Server.mainJwt() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func Test_createJwtToken(t *testing.T) {
-	type args struct {
-		user model.User
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := createJwtToken(tt.args.user)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("createJwtToken() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("createJwtToken() = %v, want %v", got, tt.want)
 			}
 		})
 	}
